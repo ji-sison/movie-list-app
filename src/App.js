96a05f75b22048ce0d1728474925474a1ad0befe
Ji Sison <jisison.dev@gmail.com>
2023-02-26 11:17:52 +0800

    API test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import { useEffect } from 'react';
+
+//API url, hidden for security reasons
+
 
 const App = () => {
+
+    const searchMovie = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json(); 
+
+        console.log(data.Search);
+    }
+
+    useEffect(()=> {
+        searchMovie("Spiderman")
+    }, []);
+
     return ( 
         <h1>Movie App </h1>
     )
