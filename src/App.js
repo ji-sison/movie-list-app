7d47e9487140fb98a9b0c49d61d8c01aa979dbc7
Ji Sison <jisison.dev@gmail.com>
2023-02-26 14:24:51 +0800

    API test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { useEffect } from 'react';
 
-//API url, hidden for security reasons
+//API from www.omdbapi.com
+//API key, hidden for security reasons
+const API_URL = 'http://www.omdbapi.com?apikey='
 
+const movie = {
+    "Title": "Avengers: Endgame",
+    "Year": "2019",
+    "imdbID": "tt2586634",
+    "Type": "Movie",
+    "Poster": "N/A"
+
+}
 
 const App = () => {
 
@@ -18,7 +28,36 @@ const App = () => {
     }, []);
 
     return ( 
-        <h1>Movie App </h1>
+        <div className="app">
+            <h1>MovieDB</h1>
+
+            <div className="search">
+                <input
+                    placeholder='Search for movies'
+                    value="Avengers"
+                    onChange={() => {}}
+                />
+            </div>
+
+            <div className="container">
+                <div className="movies">
+                    <div>
+                        <p>{movie.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}/>
+                    </div>
+                </div>
+                
+            </div>
+
+            <div>
+                <span> {movie.Type} </span>
+                <h3> {movie.Title} </h3>
+            </div>
+
+        </div>
     )
 
 }
